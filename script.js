0dd4268ca1e047477615444eb64eb00600dd81dc
Tazochig <chigladzetazo@gmail.com>
2024-11-08 07:21:37 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1;
+// 2;
+// const fruits = ["apple", "banana", "orange", "grape"];
+// fruits.forEach((fruits) => {
+//   console.log(fruits);
+// });
+// 3;
+// const numbers = [1, 2, 3, 4, 5];
+// let jami = 0;
+// numbers.forEach(function (numbers) {
+//   jami += numbers;
+// });
+// console.log(jami);
+// 4;
+// const names = ["tazo", "gio", "nika", "nadira"];
+// 5;
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// numbers.forEach(function (num) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//   }
+// });
+6;
