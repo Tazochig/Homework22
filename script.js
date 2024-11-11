c40cadb4cd722b032e1cc719de05e2bb5a370511
Tazochig <chigladzetazo@gmail.com>
2024-11-11 06:59:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // 1;
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// numbers.forEach((number, index) => {
+//   return (numbers[index] = number * number);
+// });
+// console.log(numbers)
 // 2;
 // const fruits = ["apple", "banana", "orange", "grape"];
 // fruits.forEach((fruits) => {
@@ -12,7 +17,7 @@
 // });
 // console.log(jami);
 // 4;
-// const names = ["tazo", "gio", "nika", "nadira"];
+// const names = ["tazo", "gio", "nika", "nadira"]; es ver gavige
 // 5;
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // numbers.forEach(function (num) {
@@ -20,4 +25,107 @@
 //     console.log(num);
 //   }
 // });
-6;
+// 6;
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let changednumbers = arr.map((number) => {
+//   return number * number;
+// });
+// console.log(changednumbers);
+// 7;
+// let names = ["Tazo", "Giorgi", "Nikoloz", "Luka", "Davit"];
+// let saxelebi = names.map((name) => name.toLowerCase());
+
+// console.log(saxelebi);
+// 8;
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let changednumbers = arr.map((number) => {
+//     return (number += 10);
+//   return number * 10 ;
+// });
+// console.log(changednumbers);
+//   piroba ver gavige da an erti ikneba an meore wesit
+// 9;
+// const students = [
+//   { name: "Tazo", score: 85 },
+//   { name: "Giorgi", score: 92 },
+//   { name: "Nikoloz", score: 78 },
+//   { name: "Luka", score: 88 },
+//   { name: "Davit", score: 95 },
+// ];
+// let saxelebi = students.map((index) => {
+//   return index.name;
+// });
+// console.log(saxelebi);
+
+// 10;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let changedNumbers = arr.map((num) => {
+//   if (num % 2 === 0) {
+//     return num * 2;
+//   } else {
+//     return num;
+//   }
+// });
+// console.log(changedNumbers);
+// 11;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let changedNumbers = arr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(changedNumbers)
+// 12;
+// let names = ["ini", "iani", "gela", "vaxtangi"];
+// let filteredNames = names.filter((saxelebi) => {
+//     return saxelebi.length > 5
+// })
+// console.log(filteredNames)
+// 13;
+// let arr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let changedNumbers = arr.filter((num) => {
+//   return num > 0;
+// });
+// console.log(changedNumbers);
+// 14;
+// const products = [
+//   { name: "Coffee", Price: 105 },
+//   { name: "Tea", Price: 55 },
+//   { name: "Sugar", Price: 33 },
+//   { name: "Milk", Price: 271 },
+//   { name: "Bread", Price: 84 },
+// ];
+// let FilteredProducts = products.filter((index) => {
+//   return index.Price > 100;
+// });
+// console.log(FilteredProducts);
+// 15;
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let changedNumbers = arr.filter((num) => {
+//   return num > 50;
+// });
+// console.log(changedNumbers);
+// 16;
+// let cifri = 1;
+// while (cifri < 11) {
+//   console.log(cifri);
+//   cifri++;
+// }
+// 17;
+// let cifrebi = [1, 5, 2, 7, 12, 53, 21, 42];
+// let jami = 0
+// while () {
+
+// }
+// console.log(vera )
+// 18;
+// let cifri = 1;
+// while (cifri <= 20) {
+//   console.log(cifri);
+//   cifri += 2;
+// }
+// 19;
+// let cifri = 20;
+// while (cifri > 9) {
+//   console.log(cifri);
+//   cifri--;
+// }
+20;
